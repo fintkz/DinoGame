f923f881b64d923e8a5c38d30fb41b97d7fed5e5
fintkz <nilesh23.fin@gmail.com>
2023-07-12 15:45:42 +0530

    fix obstacle height

--- scripts/obstacle.js
+++ scripts/obstacle.js
@@ -2,7 +2,7 @@ class Obstacle {
     constructor(gap = 150) {
         this.r = 50; // Fixed size for the obstacles
         this.x = width;
-        this.y = height - this.r - gap;
+        this.y = height - this.r - gap - 10; // Adjusted y position to align obstacles with the dino
         this.speed = 5; // Initial speed of the obstacle
     }
 
